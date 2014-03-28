dee2f55526094a792510c8cc2026856d01bb9015
Juan Ruiz <invitado@perdidita.(none)>
2014-03-28 16:37:56 +0000

    Finish CarrierWaveDirect

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,11 @@ CarrierWave.configure do |config|
 #    :host                   => 's3.amazonaws.com',             # optional, defaults to nil
 #    :endpoint               => 'https://s3-us-west-2.amazonaws.com' # optional, defaults to nil
   }
-  config.fog_directory  = 'shoeaddict'                       # required ¿is this the bucket name?
+  config.fog_directory  = 'shoeaddict'                       # required --> this is the bucket name
   config.fog_public     = true                                    # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  config.will_include_content_type = true
+  config.default_content_type = 'image/jpg'
+  config.allowed_content_types = %w(image/jpeg  image/jpg image/png image/gif)
 end
+
